8d7e0d90dd683cc2ceb8f7f059089181ea76ec0d
Pradhit Ongole <pradhitongole@gmail.com>
2020-10-07 22:11:23 +0530

    .

--- basic.py
+++ basic.py
@@ -4,9 +4,6 @@ from matplotlib import pyplot as plt
 
 img = cv2.imread('messi.jpg')
 
-<<<<<<< HEAD
+
 plt.imshow(img)
-=======
-cv2.imshow("Output",img)
-cv2.waitKey(1000)
->>>>>>> 8e5eba6a8212271c6a1fa5e09582e6312ba867e8
+
